0a807f918f7857dbd3f44ea8e8038a88aef19639
mtfcd <mtfcd_cc@126.com>
2022-03-20 21:35:38 +0800

    minor bug

--- src/App.tsx
+++ src/App.tsx
@@ -163,7 +163,7 @@ function App() {
         setIsStatsModalOpen(true)
       }, GAME_LOST_INFO_DELAY)
     }
-  }, [isGameWon, isGameLost, showSuccessAlert])
+  }, [isGameWon, isGameLost, showSuccessAlert, wordLength])
 
   const onChar = (value: string) => {
     if (
@@ -268,7 +268,7 @@ function App() {
         setIsCreateModalOpen={setIsCreateModalOpen}
       />
       <div className="pt-2 px-1 pb-8 md:max-w-7xl w-full mx-auto sm:px-6 lg:px-8 flex flex-col grow">
-        <div className="pb-6 grow">
+        <div className="pb-6 half-grow">
           <Grid
             guesses={guesses}
             guessRes={checks}
